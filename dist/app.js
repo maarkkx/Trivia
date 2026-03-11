ab34d83fdfa2e6ed34bf047c1804a08e11f1ea2c
Álvaro Masedo <alvaromasedoperez@gmail.com>
2026-03-11 16:22:40 +0100

    Refactor app setup to serve static files and handle SPA routing

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Backend funcionando");
+// Middleware
+app.use(express_1.default.json());
+// Servir archivos estáticos de public/
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+// Servir archivos compilados del cliente (JS compilado desde TS)
+app.use(express_1.default.static(path_1.default.join(__dirname, "../dist/client")));
+// Si no es una llamada API, servir index.html (Single Page App)
+app.get(/.*/, (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../index.html"));
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend funcionando");
+// Middleware
+app.use(express.json());
+
+// Servir archivos estáticos de public/
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Servir archivos compilados del cliente (JS compilado desde TS)
+app.use(express.static(path.join(__dirname, "../dist/client")));
+
+// Si no es una llamada API, servir index.html (Single Page App)
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 export default app;
